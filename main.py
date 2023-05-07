d078877fa594fa93560b1557c632b97088b89ae9
tyler c <tylercain390@gmail.com>
2023-05-07 11:12:11 -0600

    Build 0.3
    Added view of individual days morale

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 # CONTANTS
 Morale1 = 100
 Morale2 = 100
+Days = 0
 
 
 def Roll():
@@ -18,16 +19,28 @@ def Battle():
     Morale1 = Morale1 - (0.5 * Roll())
     Morale2 = Morale2 - (0.5 * Roll())
 
+    #if random.choice([True, False]):
+       # Morale1 = Morale1 - morale_loss1
+       # Morale2 = Morale2 - morale_loss2
+    #else:
+       # Morale2 = Morale2 - morale_loss2
+       # Morale1 = Morale1 - morale_loss1
+
 
 while Morale1 > 0 and Morale2 > 0:
-    Battle() 
+    Battle()
+    print(f"Day {Days}")
+    print(f"Army 1 Morale: {Morale1}")
+    print(f"Army 2 Morale: {Morale2}")
+    print("")
+    Days += 1
 
 
 if Morale1 <= 0:
-    print("Side 2 wins!")
+    print("Army 2 wins!")
     print(f"Morale: {Morale2}")
 elif Morale2 <= 0:
-    print("Side 1 wins!")
+    print("Army 1 wins!")
     print(f"Morale: {Morale1}")
 
 sys.exit()
